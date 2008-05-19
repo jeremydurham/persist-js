8ad38dbbb631ef0ffea9e503bbb1fca01ccff077
pabs@pablotron.org <none@none>
2008-05-19 02:14:06 -0400

    add test.js

---
+++ test/test.js
@@ -0,0 +1,29 @@
+
+Test = {
+  get: function(id) {
+    return document.getElementById(id);
+  },
+
+  load: function() {
+    Test.store.get('some_key', function(ok, val) {
+      if (ok)
+        Test.get('data').value = val;
+    });
+  },
+
+  save: function() {
+    var val = Test.get('data').value = val;
+    Test.store.set('some_key', val);
+  },
+
+  init: function() {
+    // create new persistent store
+    Test.store = new Persist.Store('test', { 
+      domain: 'localdomain'
+    });
+
+    // attach callbacks
+    Test.get('load-btn').onclick = Test.load;
+    Test.get('save-btn').onclick = Test.save;
+  }
+};
