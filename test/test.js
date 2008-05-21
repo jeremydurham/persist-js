d0528adab3f0ffd64712fd916299fdf49951674f
pabs@pablotron.org <none@none>
2008-05-21 03:24:06 -0400

    add second data store

--- test/test.js
+++ test/test.js
@@ -16,14 +16,31 @@ Test = {
     Test.store.set('some_key', val);
   },
 
+  load2: function() {
+    Test.store2.get('some_key', function(ok, val) {
+      if (ok)
+        Test.get('data2').value = val;
+    });
+  },
+
+  save2: function() {
+    var val = Test.get('data2').value;
+    Test.store2.set('some_key', val);
+  },
+
   init: function() {
     // create new persistent store
     Test.store = new Persist.Store('test');
+    Test.store2 = new Persist.Store('test2');
 
     Test.get('type').innerHTML = Persist.type;
 
     // attach callbacks
     Test.get('load-btn').onclick = Test.load;
     Test.get('save-btn').onclick = Test.save;
+
+    // attach callbacks
+    Test.get('load2-btn').onclick = Test.load2;
+    Test.get('save2-btn').onclick = Test.save2;
   }
 };
