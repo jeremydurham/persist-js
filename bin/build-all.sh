e675a146d85117e9f601e18f55b080543b55062e
pabs@pablotron.org <none@none>
2009-02-03 13:51:28 -0500

    add comments to bin/build*.sh files

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+#
+# This script is executed in a pre-commit hook to automagically
+# regenerate the top-level persist-all-min.js file.
+#
+
 cat extras/swfobject.js src/persist.js | jsmin > persist-all-min.js
--- bin/build-min.sh
+++ bin/build-min.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+#
+# This script is executed in a pre-commit hook to automagically
+# regenerate the top-level persist-min.js file.
+#
+
 jsmin < src/persist.js > persist-min.js
