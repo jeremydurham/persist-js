693a3ad0ab6436d0ed94fd51e9b9ac6856ea6dcb
Jeremy Durham <jeremydurham@gmail.com>
2009-08-25 21:23:43 -0400

    Removed minified version of easycookie and added fix for IE6 and localhost not storing cookie correctly

--- src/persist.js
+++ src/persist.js
@@ -81,24 +81,274 @@
  */
 Persist = (function() {
   var VERSION = '0.2.0', P, B, esc, init, empty, ec;
+  
+  ec = (function() {
+    var EPOCH = 'Thu, 01-Jan-1970 00:00:01 GMT',
+        // milliseconds per day
+        RATIO = 1000 * 60 * 60 * 24,
+        // keys to encode 
+        KEYS = ['expires', 'path', 'domain'],
+        // wrappers for common globals
+        esc = escape, un = unescape, doc = document,
+        me; 
 
-  // easycookie 0.2.1 (pre-minified)
-  // (see http://pablotron.org/software/easy_cookie/)
-  ec = (function(){var EPOCH='Thu, 01-Jan-1970 00:00:01 GMT',RATIO=1000*60*60*24,KEYS=['expires','path','domain'],esc=escape,un=unescape,doc=document,me;var get_now=function(){var r=new Date();r.setTime(r.getTime());return r;}
-var cookify=function(c_key,c_val){var i,key,val,r=[],opt=(arguments.length>2)?arguments[2]:{};r.push(esc(c_key)+'='+esc(c_val));for(i=0;i<KEYS.length;i++){key=KEYS[i];if(val=opt[key])
-r.push(key+'='+val);}
-if(opt.secure)
-r.push('secure');return r.join('; ');}
-var alive=function(){var k='__EC_TEST__',v=new Date();v=v.toGMTString();this.set(k,v);this.enabled=(this.remove(k)==v);return this.enabled;}
-me={set:function(key,val){var opt=(arguments.length>2)?arguments[2]:{},now=get_now(),expire_at,cfg={};if(opt.expires){var expires=opt.expires;expires*=RATIO;cfg.expires=new Date(now.getTime()+expires);cfg.expires=cfg.expires.toGMTString();}
-var keys=['path','domain','secure'];for(i=0;i<keys.length;i++)
-if(opt[keys[i]])
-cfg[keys[i]]=opt[keys[i]];var r=cookify(key,val,cfg);doc.cookie=r;return val;},has:function(key){key=esc(key);var c=doc.cookie,ofs=c.indexOf(key+'='),len=ofs+key.length+1,sub=c.substring(0,key.length);return((!ofs&&key!=sub)||ofs<0)?false:true;},get:function(key){key=esc(key);var c=doc.cookie,ofs=c.indexOf(key+'='),len=ofs+key.length+1,sub=c.substring(0,key.length),end;if((!ofs&&key!=sub)||ofs<0)
-return null;end=c.indexOf(';',len);if(end<0)
-end=c.length;return un(c.substring(len,end));},remove:function(k){var r=me.get(k),opt={expires:EPOCH};doc.cookie=cookify(k,'',opt);return r;},keys:function(){var c=doc.cookie,ps=c.split('; '),i,p,r=[];for(i=0;i<ps.length;i++){p=ps[i].split('=');r.push(un(p[0]));}
-return r;},all:function(){var c=doc.cookie,ps=c.split('; '),i,p,r=[];for(i=0;i<ps.length;i++){p=ps[i].split('=');r.push([un(p[0]),un(p[1])]);}
-return r;},version:'0.2.1',enabled:false};me.enabled=alive.call(me);return me;}());
+    // private methods
 
+    /*
+     * Get the current time.
+     *
+     * This method is private.
+     */
+    var get_now = function() {
+      var r = new Date();
+      r.setTime(r.getTime());
+      return r;
+    }
+
+    /*
+     * Convert the given key/value pair to a cookie.
+     *
+     * This method is private.
+     */
+    var cookify = function(c_key, c_val /*, opt */) {
+       var i, key, val, r = [],
+           opt = (arguments.length > 2) ? arguments[2] : {};
+
+      // add key and value
+      r.push(esc(c_key) + '=' + esc(c_val));
+
+      // iterate over option keys and check each one
+      for (i = 0; i < KEYS.length; i++) {
+        key = KEYS[i];
+        if (val = opt[key])
+          r.push(key + '=' + val);
+      }
+
+      // append secure (if specified)
+      if (opt.secure)
+        r.push('secure');
+
+      // build and return result string
+      return r.join('; ');
+    }
+
+    /*
+     * Check to see if cookies are enabled.
+     *
+     * This method is private.
+     */
+    var alive = function() {
+      var k = '__EC_TEST__', 
+          v = new Date();
+
+      // generate test value
+      v = v.toGMTString();
+
+      // set test value
+      this.set(k, v);
+
+      // return cookie test
+      this.enabled = (this.remove(k) == v);
+      return this.enabled;
+    }
+
+    // public methods
+
+    // build return object
+    me = {
+      /*
+       * Set a cookie value.
+       *
+       * Examples:
+       *
+       *   // simplest-case
+       *   EasyCookie.set('test_cookie', 'test_value');
+       *
+       *   // more complex example
+       *   EasyCookie.set('test_cookie', 'test_value', {
+       *     // expires in 13 days
+       *     expires: 13,
+       *
+       *     // restrict to given domain
+       *     domain: 'foo.example.com',
+       *
+       *     // restrict to given path
+       *     path: '/some/path',
+       *
+       *     // secure cookie only
+       *     secure: true
+       *   });
+       *
+       */
+      set: function(key, val /*, opt */) {
+        var opt = (arguments.length > 2) ? arguments[2] : {}, 
+            now = get_now(),
+            expire_at,
+            cfg = {};
+
+        // if expires is set, convert it from days to milliseconds
+        if (opt.expires) {
+          // Needed to assign to a temporary variable because of pass by reference issues
+          var expires = opt.expires * RATIO;
+
+          // set cookie expiration date
+          cfg.expires = new Date(now.getTime() + expires);
+          cfg.expires = cfg.expires.toGMTString();
+        }
+
+        // set remaining keys
+        var keys = ['path', 'domain', 'secure'];
+        for (i = 0; i < keys.length; i++)
+          if (opt[keys[i]])
+            cfg[keys[i]] = opt[keys[i]];
+
+        var r = cookify(key, val, cfg);
+        doc.cookie = r;
+
+        return val;
+      },
+
+      /*
+       * Check to see if the given cookie exists.
+       *
+       * Example:
+       *
+       *   val = EasyCookie.get('test_cookie');
+       *
+       */
+      has: function(key) {
+        key = esc(key);
+
+        var c = doc.cookie,
+            ofs = c.indexOf(key + '='),
+            len = ofs + key.length + 1,
+            sub = c.substring(0, key.length);
+
+        // check to see if key exists
+        return ((!ofs && key != sub) || ofs < 0) ? false : true;
+      },
+
+      /*
+       * Get a cookie value.
+       *
+       * Example:
+       *
+       *   val = EasyCookie.get('test_cookie');
+       *
+       */
+      get: function(key) {
+        key = esc(key);
+
+        var c = doc.cookie, 
+            ofs = c.indexOf(key + '='),
+            len = ofs + key.length + 1,
+            sub = c.substring(0, key.length),
+            end;
+
+        // check to see if key exists
+        if ((!ofs && key != sub) || ofs < 0)
+          return null;
+
+        // grab end of value
+        end = c.indexOf(';', len);
+        if (end < 0) 
+          end = c.length;
+
+        // return unescaped value
+        return un(c.substring(len, end));
+      },
+
+      /*
+       * Remove a preset cookie.  If the cookie is already set, then
+       * return the value of the cookie.
+       *
+       * Example:
+       *
+       *   old_val = EasyCookie.remove('test_cookie');
+       *
+       */
+      remove: function(k) {
+        var r = me.get(k), 
+            opt = { expires: EPOCH };
+
+        // delete cookie
+        doc.cookie = cookify(k, '', opt);
+
+        // return value
+        return r;
+      },
+
+      /*
+       * Get a list of cookie names.
+       *
+       * Example:
+       *
+       *   // get all cookie names
+       *   cookie_keys = EasyCookie.keys();
+       *
+       */
+      keys: function() {
+        var c = doc.cookie, 
+            ps = c.split('; '),
+            i, p, r = [];
+
+        // iterate over each key=val pair and grab the key
+        for (i = 0; i < ps.length; i++) {
+          p = ps[i].split('=');
+          r.push(un(p[0]));
+        }
+
+        // return results
+        return r;
+      },
+
+      /*
+       * Get an array of all cookie key/value pairs.
+       *
+       * Example:
+       *
+       *   // get all cookies
+       *   all_cookies = EasyCookie.all();
+       *
+       */
+      all: function() {
+        var c = doc.cookie, 
+            ps = c.split('; '),
+            i, p, r = [];
+
+        // iterate over each key=val pair and grab the key
+        for (i = 0; i < ps.length; i++) {
+          p = ps[i].split('=');
+          r.push([un(p[0]), un(p[1])]);
+        }
+
+        // return results
+        return r;
+      },
+
+      /* 
+       * Version of EasyCookie
+       */
+      version: '0.2.1',
+
+      /*
+       * Are cookies enabled?
+       *
+       * Example:
+       *
+       *   have_cookies = EasyCookie.enabled
+       *
+       */
+      enabled: false
+    };
+
+    // set enabled attribute
+    me.enabled = alive.call(me);
+
+    // return self
+    return me;
+  }());
+  
   // wrapper for Array.prototype.indexOf, since IE doesn't have it
   var index_of = (function() {
     if (Array.prototype.indexOf)
@@ -143,12 +393,12 @@ return r;},version:'0.2.1',enabled:false};me.enabled=alive.call(me);return me;}(
     search_order: [
       // TODO: air
       'localstorage',
-      'whatwg_db', 
       'globalstorage', 
       'gears',
+      'whatwg_db', 
+      'cookie',
       'ie', 
-      'flash',
-      'cookie'
+      'flash'
     ],
 
     // valid name regular expression
@@ -704,7 +954,7 @@ return r;},version:'0.2.1',enabled:false};me.enabled=alive.call(me);return me;}(
 
         get: function(key, fn, scope) {
           var val;
-
+          
           // expand key 
           key = this.key(key);
 
@@ -930,11 +1180,14 @@ return r;},version:'0.2.1',enabled:false};me.enabled=alive.call(me);return me;}(
       o = o || {};
       this.name = name;
 
-      // get domain (XXX: does this localdomain fix work?)
+      // get domain (XXX: does this localdomain fix work?)      
       o.domain = o.domain || location.host || 'localhost';
       
       // strip port from domain (XXX: will this break ipv6?)
       o.domain = o.domain.replace(/:\d+$/, '')
+      
+      // Cleanup for IE6
+      o.domain = (o.domain == 'localhost') ? '' : o.domain;
 
       // append localdomain to domains w/o '."
       // (see https://bugzilla.mozilla.org/show_bug.cgi?id=357323)
